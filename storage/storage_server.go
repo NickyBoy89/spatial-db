a4620b99ce799467b9e790fdda96a517c42526dd
Nicholas Novak <34256932+NickyBoy89@users.noreply.github.com>
2023-12-05 14:38:19 -0800

    fix: Updated some of the methods in the storage server to be correct

--- storage/storage_server.go
+++ storage/storage_server.go
@@ -6,10 +6,12 @@ type StorageServer interface {
 	// Individual operations
 	SetStorageRoot(path string)
 
-	// Individual block-level interactions
+	// Block-level interactions
 	ChangeBlock(targetState world.BlockID, world_position world.BlockPos) error
+	ReadBlockAt(pos world.BlockPos) (world.BlockID, error)
+
+	// Region-level interactions
 	ChangeBlockRange(targetState world.BlockID, start, end world.BlockPos) error
-	ReadBlockAt(pos world.BlockPos) error
 
 	// Network-level operations
 	ReadChunkAt(pos world.ChunkPos) error
